aae8a6266356cb11a2c38ba3dc9fe5b40c58e59b
Gheorghe Darle <darlegheorghe@yahoo.ro>
2021-02-20 16:09:13 +0200

    feat(floating-button): add floating button

--- ToDoApp/ToDoApp/ToDoApp/ViewModels/WelcomePageViewModel.cs
+++ ToDoApp/ToDoApp/ToDoApp/ViewModels/WelcomePageViewModel.cs
@@ -13,7 +13,21 @@ namespace ToDoApp.ViewModels
         {
             TaskList = new ObservableCollection<string>()
             {
-                "Task Number 1", "Task Number 2"
+                "Task Number 1",
+                "Task Number 2",
+                "Task Number 3",
+                "Task Number 4",
+                "Task Number 5",
+                "Task Number 6",
+                "Task Number 7",
+                "Task Number 8",
+                "Task Number 9",
+                "Task Number 10",
+                "Task Number 11",
+                "Task Number 12",
+                "Task Number 13",
+                "Task Number 14",
+                "Task Number 15"
             };
         }
 
