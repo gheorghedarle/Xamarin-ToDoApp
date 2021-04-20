0183eed0e1a2b5cfbadd8f1a8224b264e409c1a4
Gheorghe Darle <darlegheorghe@yahoo.ro>
2021-04-20 22:08:01 +0300

    feat(add-task): add close command

--- ToDoApp/ToDoApp/ToDoApp/ViewModels/Dialogs/AddTaskDialogViewModel.cs
+++ ToDoApp/ToDoApp/ToDoApp/ViewModels/Dialogs/AddTaskDialogViewModel.cs
@@ -1,25 +1,34 @@
 ﻿using Prism.Navigation;
 using Prism.Services.Dialogs;
 using System;
+using Xamarin.Forms;
 
 namespace ToDoApp.ViewModels.Dialogs
 {
     public class AddTaskDialogViewModel : BaseViewModel, IDialogAware
     {
+        public Command CloseCommand { get; set; }
+
         #region Constructors
 
         public AddTaskDialogViewModel(
             INavigationService navigationService) : base(navigationService)
-        { }
+        {
+            CloseCommand = new Command(CloseCommandHandler);
+        }
 
         #endregion
+
+        private void CloseCommandHandler()
+        {
+            RequestClose(null);
+        }
 
         #region Dialog
 
         public event Action<IDialogParameters> RequestClose;
 
-        public bool CanCloseDialog()
-        { return true; }
+        public bool CanCloseDialog() => true;
 
         public void OnDialogClosed()
         { }
