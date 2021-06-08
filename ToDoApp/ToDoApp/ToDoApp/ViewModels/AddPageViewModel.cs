c793d81dbf3320cc24c8c334c2c240457b4a1173
Gheorghe Darle <darlegheorghe@yahoo.ro>
2021-06-08 18:12:54 +0300

    wip(add-page): add type to add page

--- ToDoApp/ToDoApp/ToDoApp/ViewModels/AddPageViewModel.cs
+++ ToDoApp/ToDoApp/ToDoApp/ViewModels/AddPageViewModel.cs
@@ -4,8 +4,12 @@ namespace ToDoApp.ViewModels
 {
     public class AddPageViewModel : BaseViewModel
     {
+        public string Type { get; set; }
+
         public AddPageViewModel(
             INavigationService navigationService) : base(navigationService)
-        { }
+        {
+            Type = "task";
+        }
     }
 }
