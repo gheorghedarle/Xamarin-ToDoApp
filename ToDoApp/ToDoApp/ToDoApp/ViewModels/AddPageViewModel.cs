5fda9c6bcb455b46ed4deff937892670a751f3b3
Gheorghe Darle <darlegheorghe@yahoo.ro>
2021-06-09 10:48:19 +0300

    wip(add-page): add visual state for type

--- ToDoApp/ToDoApp/ToDoApp/ViewModels/AddPageViewModel.cs
+++ ToDoApp/ToDoApp/ToDoApp/ViewModels/AddPageViewModel.cs
@@ -1,15 +1,44 @@
 ﻿using Prism.Navigation;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace ToDoApp.ViewModels
 {
     public class AddPageViewModel : BaseViewModel
     {
+
+        #region Properties
+
         public string Type { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand ChangeTypeCommand { get; set; }
+
+
+        #endregion
+
+        #region Constructors
 
         public AddPageViewModel(
             INavigationService navigationService) : base(navigationService)
         {
+            ChangeTypeCommand = new Command<string>(ChangeTypeCommandHandler);
+
             Type = "task";
         }
+
+        #endregion
+
+        #region Command Handlers
+
+        private void ChangeTypeCommandHandler(string type)
+        {
+            Type = type;
+        }
+
+        #endregion
     }
 }
