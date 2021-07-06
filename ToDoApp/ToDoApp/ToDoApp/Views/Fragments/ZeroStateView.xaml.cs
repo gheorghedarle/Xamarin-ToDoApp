5d328d99e02478e8a9d1cdc869a5bd343a9c17f6
Gheorghe Darle <darlegheorghe@yahoo.ro>
2021-07-06 13:32:08 +0300

    fix(task): move zero state to separate view

---
+++ ToDoApp/ToDoApp/ToDoApp/Views/Fragments/ZeroStateView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoApp.Views.Fragments
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ZeroStateView : StackLayout
+    {
+        public ZeroStateView()
+        {
+            InitializeComponent();
+        }
+    }
+}
