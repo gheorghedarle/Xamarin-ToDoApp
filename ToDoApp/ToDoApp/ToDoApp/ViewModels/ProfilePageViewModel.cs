68e17297320f36e5099f38311d3b8b1154a311dc
Gheorghe Darle <darlegheorghe@yahoo.ro>
2021-07-30 17:55:45 +0300

    wip(profile-page): add command handlers on vm

--- ToDoApp/ToDoApp/ToDoApp/ViewModels/ProfilePageViewModel.cs
+++ ToDoApp/ToDoApp/ToDoApp/ViewModels/ProfilePageViewModel.cs
@@ -1,11 +1,38 @@
 ﻿using Prism.Navigation;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace ToDoApp.ViewModels
 {
-    class ProfilePageViewModel : BaseViewModel
+    public class ProfilePageViewModel : BaseViewModel
     {
+        #region Commands
+
+        public ICommand BackCommand { get; set; }
+        public ICommand DarkModeToggleCommand { get; set; }
+        public ICommand HideDoneToggleCommand { get; set; }
+
+        #endregion
+
+        #region Constructors
+
         public ProfilePageViewModel(INavigationService navigationService) : base(navigationService)
         {
+            BackCommand = new Command(BackCommandHandler);
+            DarkModeToggleCommand = new Command(DarkModeToggleCommandHandler);
+            HideDoneToggleCommand = new Command(HideDoneToggleCommandHandler);
         }
+
+        #endregion
+
+        #region Command Handlers
+
+        private void DarkModeToggleCommandHandler()
+        { }
+
+        private void HideDoneToggleCommandHandler()
+        { }
+
+        #endregion
     }
 }
