5a77392b45b0abed48b5845eddbb231780c667c8
Gheorghe Darle <darlegheorghe@yahoo.ro>
2021-12-10 11:14:08 +0200

    feat(icon): add app icon for android

--- ToDoApp/ToDoApp/ToDoApp.Android/Resources/Resource.designer.cs
+++ ToDoApp/ToDoApp/ToDoApp.Android/Resources/Resource.designer.cs
@@ -28354,10 +28354,16 @@ namespace ToDoApp.Droid
 			public const int icon = 2131492864;
 			
 			// aapt resource value: 0x7F0C0001
-			public const int icon_round = 2131492865;
+			public const int icon_adaptive_back = 2131492865;
 			
 			// aapt resource value: 0x7F0C0002
-			public const int launcher_foreground = 2131492866;
+			public const int icon_adaptive_fore = 2131492866;
+			
+			// aapt resource value: 0x7F0C0003
+			public const int icon_round = 2131492867;
+			
+			// aapt resource value: 0x7F0C0004
+			public const int launcher_foreground = 2131492868;
 			
 			static Mipmap()
 			{
